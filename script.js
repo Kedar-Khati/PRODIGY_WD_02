3f6c11b8110f1df9291b781515b83c1403f79249
Kedar Khati <Kedar K>
2024-06-29 17:10:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,116 @@
+let startBtn = document.getElementById('start'); 
+let stopBtn = document.getElementById('stop'); 
+let resetBtn = document.getElementById('reset'); 
+let lapBtn = document.getElementById('lap'); 
+var l1 = document.getElementById("lap1");
+var l2 = document.getElementById("lap2");
+var l3 = document.getElementById("lap3");
+var l4 = document.getElementById("lap4");
+var l5 = document.getElementById("lap5");
+
+let hour = 0; 
+let minute = 0; 
+let second = 0; 
+let count = 0; 
+let ctr = 0;
+
+
+startBtn.addEventListener('click', function () { 
+	timer = true; 
+	stopWatch(); 
+}); 
+
+stopBtn.addEventListener('click', function () { 
+	timer = false; 
+});
+
+lapBtn.addEventListener('click', function () { 
+	lap(); 
+}); 
+
+resetBtn.addEventListener('click', function () { 
+	timer = false; 
+	hour = 0; 
+	minute = 0; 
+	second = 0; 
+	ctr = 0; 
+	count = 0;
+	l1.innerHTML = "";
+	l2.innerHTML = "";
+	l3.innerHTML = "";
+	l4.innerHTML = "";
+	l5.innerHTML = "";
+	document.getElementById('hr').innerHTML = "00"; 
+	document.getElementById('min').innerHTML = "00"; 
+	document.getElementById('sec').innerHTML = "00"; 
+	document.getElementById('count').innerHTML = "00";
+}); 
+
+let hrString = hour; 
+let minString = minute; 
+let secString = second; 
+let countString = count;
+
+function stopWatch() { 
+	if (timer) { 
+		count++;
+        
+		if (count == 100) { 
+			second++; 
+			count = 0; 
+		} 
+
+		if (second == 60) { 
+			minute++; 
+			second = 0; 
+		} 
+
+		if (minute == 60) { 
+			hour++; 
+			minute = 0; 
+			second = 0; 
+		}  
+
+		hrString = hour; 
+		minString = minute; 
+		secString = second; 
+		countString = count;
+
+		if (hour < 10) { 
+			hrString = "0" + hrString; 
+		} 
+
+		if (minute < 10) { 
+			minString = "0" + minString; 
+		} 
+
+		if (second < 10) { 
+			secString = "0" + secString; 
+		} 
+
+		if (count < 10) { 
+			countString = "0" + countString; 
+		} 
+
+
+		document.getElementById('hr').innerHTML = hrString; 
+		document.getElementById('min').innerHTML = minString; 
+		document.getElementById('sec').innerHTML = secString; 
+		document.getElementById('count').innerHTML = countString;
+		setTimeout(stopWatch, 10); 
+	} 
+}
+
+function lap() {
+    ctr++;
+    if(ctr%5==1)
+        l1.innerHTML="Lap "+ ctr+ ":  " + hrString+":  " +minString+":  " +secString+":  " +countString;
+    if (ctr%5==2)
+        l2.innerHTML="Lap "+ ctr+ ":  "+ hrString+":  " +minString+":  " +secString+":  " +countString;
+    if (ctr%5==3)
+        l3.innerHTML="Lap "+ ctr+ ":  "+ hrString+":  " +minString+":  " +secString+":  " +countString;
+    if (ctr%5==4)
+        l4.innerHTML="Lap "+ ctr+ ":  "+ hrString+":  " +minString+":  " +secString+":  " +countString;
+    if (ctr%5==0)
+        l5.innerHTML="Lap "+ ctr+ ":  "+ hrString+":  " +minString+":  " +secString+":  " +countString;
+}
